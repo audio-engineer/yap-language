60e148e256c8dc4e315dbda020d90b66ccf5b83c
Martin Kedmenec <martin.kedmenec@outlook.com>
2025-03-02 23:27:26 +0100

    Fix #13. Add initial code for print function

--- src/lexer.h
+++ src/lexer.h
@@ -6,20 +6,33 @@ enum {
 };
 
 typedef enum TokenType {
+  kEOF,
+  kId,
   kNumber,
   kPlus,
   kMinus,
-  kMultiplication,
-  kDivision,
+  kStar,
+  kSlash,
   kAssignment,
   kSemicolon,
+  kLeftParenthesis,
+  kRightParenthesis,
+  kQuotationMark,
+  kIf,
   kPrint
 } TokenType;
 
 typedef struct Token {
-  TokenType token_type;
+  TokenType type;
   int value;
   char text[kTokenTextBufferSize];
 } Token;
+
+// NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables)
+extern const char* source_code;
+extern Token token;
+// NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
+
+void ConsumeNextToken();
 
 #endif  // LEXER_H
---
+++ src/parser.h
@@ -0,0 +1,6 @@
+#ifndef PARSER_H
+#define PARSER_H
+
+void ParseProgram(const char* source_code_parameter);
+
+#endif  // PARSER_H
