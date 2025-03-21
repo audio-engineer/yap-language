2e5b0ddfe10275105b3baeab8e2c9874de0084ea
Martin Kedmenec <martin.kedmenec@outlook.com>
2025-03-21 15:04:16 +0100

    Fix #94, fix #112. Add support for program mode, consistent use of puts vs printf

--- src/vm.h
+++ src/vm.h
@@ -32,10 +32,45 @@ typedef enum ConstantType {
   kTypeBoolean
 } ConstantType;
 
+#ifdef __CC65__
+enum {
+  kOpcodesSize = 128,
+  kConstantsSize = 128,
+  kStringPoolSize = 512,
+  kNumberPoolSize = 64,
+  kStackSize = 16
+};
+#else
+static constexpr int kOpcodesSize = 128;
+static constexpr int kConstantsSize = 128;
+static constexpr int kStringPoolSize = 512;
+static constexpr int kNumberPoolSize = 64;
+static constexpr int kStackSize = 16;
+#endif
+
+typedef struct Constants {
+  const void* pointer[kConstantsSize];
+  ConstantType type[kConstantsSize];
+} Constants;
+
 // NOLINTBEGIN(cppcoreguidelines-avoid-non-const-global-variables)
 extern unsigned char opcodes[];
 extern size_t opcode_index;
+
+extern Constants constants;
+extern size_t constants_index;
+
+extern char string_pool[];
+extern size_t string_pool_index;
+
+extern long number_pool[];
+extern size_t number_pool_index;
+
+extern size_t stack[];
+extern size_t stack_index;
 // NOLINTEND(cppcoreguidelines-avoid-non-const-global-variables)
+
+void ResetInterpreterState();
 
 void EmitByte(unsigned char byte);
 
