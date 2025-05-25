f5bcb190ef1261218b26b5e1047892d16323856b
Mohammed A <oh58to@student.aau.dk>
2025-05-25 14:10:49 +0200

    Fix #93. Fix #144. Added acceptance testing and more vm testing.

--- tests/vm_test.c
+++ tests/vm_test.c
@@ -31,16 +31,61 @@ void TestConditionalResult(const int expected, const char* const code) {
   ResetInterpreterState();
 }
 
-void TestPlus() { TestVMArithmetic(4, "x:int=2+2"); }
+void TestPlusOperator() { TestVMArithmetic(4, "x:int=2+2"); }
 
-void TestMinusVM() { TestVMArithmetic(1, "x:int=3-2"); }
+void TestMinusOperator() { TestVMArithmetic(1, "x:int=3-2"); }
 
 // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers,-warnings-as-errors)
-void TestMultiply() { TestVMArithmetic(6, "x:int=2*3"); }
+void TestMultiplyOperator() { TestVMArithmetic(6, "x:int=2*3"); }
 
-void TestDivide() { TestVMArithmetic(2, "x:int=4/2"); }
+void TestDivideOperator() { TestVMArithmetic(2, "x:int=4/2"); }
 
-void TestModulo() { TestVMArithmetic(1, "x:int=5%2"); }
+void TestModuloOperator() { TestVMArithmetic(1, "x:int=5%2"); }
+
+void TestEqualOperator() {
+  TestVMArithmetic(1, "x:bool = 2 == 2");
+  TestVMArithmetic(0, "x:bool = 2 == 3");
+}
+
+void TestNotEqualOperator() {
+  TestVMArithmetic(0, "x:bool = 2 != 2");
+  TestVMArithmetic(1, "x:bool = 2 != 3");
+}
+
+void TestGreaterThanOperator() {
+  TestVMArithmetic(0, "x:bool = 2 > 3");
+  TestVMArithmetic(1, "x:bool = 3 > 2");
+}
+
+void TestGreaterOrEqualsOperator() {
+  TestVMArithmetic(0, "x:bool = 2 >= 3");
+  TestVMArithmetic(1, "x:bool = 3 >= 2");
+  TestVMArithmetic(1, "x:bool = 3 >= 3");
+}
+
+void TestLessThanOperator() {
+  TestVMArithmetic(1, "x:bool = 2 < 3");
+  TestVMArithmetic(0, "x:bool = 3 < 2");
+}
+
+void TestLessOrEqualsOperator() {
+  TestVMArithmetic(1, "x:bool = 2 <= 3");
+  TestVMArithmetic(0, "x:bool = 3 <= 2");
+  TestVMArithmetic(1, "x:bool = 3 <= 3");
+}
+
+void TestOrOperator() {
+  TestVMArithmetic(1, "x:bool = true || false");
+  TestVMArithmetic(1, "x:bool = false || true");
+  TestVMArithmetic(0, "x:bool = false || false");
+}
+
+void TestAndOperator() {
+  TestVMArithmetic(1, "x:bool = true && true");
+  TestVMArithmetic(0, "x:bool = true && false");
+  TestVMArithmetic(0, "x:bool = false && true");
+  TestVMArithmetic(0, "x:bool = false && false");
+}
 
 void TestIfTrueExecutesBlock() {
   // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers,-warnings-as-errors)
@@ -107,4 +152,180 @@ void TestNestedConditionals() {
                         "  x = 999\n"
                         "endif\n"
                         "print(x)");
+}
+
+// Acceptance testing
+
+void Example1() {
+  FillProgramBufferAndParse(
+      "add: int = func(x: int, y: int) \n "
+      "ret x + y \n "
+      "endfunc \n"
+      "x:int = add(5, 6)\n "
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(11, stack[stack_index].as.number);
+
+  ResetInterpreterState();
+}
+
+void Example2() {
+  FillProgramBufferAndParse(
+      "a: int = 2 \n "
+      "a = a + 1 \n "
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(3, stack[stack_index].as.number);
+
+  ResetInterpreterState();
+}
+
+void Example3() {
+  FillProgramBufferAndParse(
+      "foo: int = func(num: int)\n"
+      "ret (num + 2) * 3 \n"
+      "endfunc\n"
+      "b: int = foo(8) \n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(30, stack[stack_index].as.number);
+
+  ResetInterpreterState();
+}
+
+void Example4() {
+  FillProgramBufferAndParse(
+      "if (true)\n"
+      "  if (false)\n"
+      "    x:str = \"foo\"\n"
+      "  else \n"
+      "    x:str = \"bar\"\n"
+      "  endif\n"
+      "endif\n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL_CHAR_ARRAY("bar", stack[stack_index].as.string, 4);
+
+  ResetInterpreterState();
+}
+
+void Example5() {
+  FillProgramBufferAndParse(
+      "if (false)\n"
+      "  if (true)\n"
+      "    x:str = \"foo\"\n"
+      "  endif \n"
+      "else \n"
+      "  x:str = bar\n"
+      "endif\n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL_CHAR_ARRAY("bar", stack[stack_index].as.string, 4);
+
+  ResetInterpreterState();
+}
+
+void Example6() {
+  FillProgramBufferAndParse(
+      "a: int = 1\n"
+      "b: int = 2\n"
+      "c: int = 3\n"
+      "b = c\n"
+      "a = b\n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(3, stack[stack_index].as.number);
+
+  ResetInterpreterState();
+}
+
+void Example7() {
+  FillProgramBufferAndParse(
+      "findGCD: int = func(a: int, b: int)\n"
+      "  if(a == 0)\n"
+      "    return b\n"
+      "  endif\n"
+      "  return findGCD(b % a, a)\n"
+      "endfunc\n"
+      "x: int = findGCD(35, 15)\n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(5, stack[stack_index].as.number);
+
+  ResetInterpreterState();
+}
+
+void Example8() {
+  FillProgramBufferAndParse(
+      "isPrime: bool = func(n: int)\n"
+      "  if(n <= 1)\n"
+      "    return false\n"
+      "  endif\n"
+      "  for(i: int = 2; i < n; i = i + 1)\n"
+      "    if(n % i == 0)\n"
+      "      return false\n"
+      "    endif\n"
+      "  endfor\n"
+      "  return true\n"
+      "endfunc\n"
+      "x: bool = isPrime(90)\n"
+      "y: bool = isPrime(97)\n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(0, stack[stack_index - 1].as.number);
+  TEST_ASSERT_EQUAL(1, stack[stack_index].as.number);
+
+  ResetInterpreterState();
+}
+
+void Example9() {
+  FillProgramBufferAndParse(
+      "fib: int = func(n: int)\n"
+      "  if(n <= 1)\n"
+      "    return n\n"
+      "  endif\n"
+      "  return fib(n-1) + fib(n-2)\n"
+      "endfunc\n"
+      "x: int = fib(10)\n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(55, stack[stack_index].as.number);
+
+  ResetInterpreterState();
+}
+
+void Example10() {
+  FillProgramBufferAndParse(
+      "factorial: int = func(n: int)\n"
+      "  if(n <= 1)\n"
+      "    return 1\n"
+      "  endif\n"
+      "  return n * factorial(n - 1)\n"
+      "endfunc\n"
+      "x: int = factorial(5)\n"
+      "run");
+
+  RunVm();
+
+  TEST_ASSERT_EQUAL(120, stack[stack_index].as.number);
+
+  ResetInterpreterState();
 }
